a83ea59f1f00765750cdb3ece8595766f76d966b
alvaritu <alvarohegon@gmail.com>
2023-05-24 08:18:45 +0200

    iteration 3

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,27 @@
+const Greetings = (props) => {
+    let greet = '';
+    switch (props.lang) {
+      case 'de':
+        greet = 'Hallo';
+        break;
+      case 'fr':
+        greet = 'Bonjour';
+        break;
+      case 'en':
+        greet = 'Hello';
+        break;
+      case 'es':
+        greet = 'Hola';
+        break;
+      default:
+        greet = 'Olá';
+    }
+  
+    return (
+      <div>
+        <p>{`${greet} ${props.children}`}</p>
+      </div>
+    );
+  };
+  
+  export default Greetings;
